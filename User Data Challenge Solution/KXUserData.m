c436d331a7636a20797e40e06e6bc793770c5931
kenwilcox <wilcoxk@gmail.com>
2014-04-18 15:08:44 -0600

    I like jpg better than jpeg

--- User Data Challenge Solution/KXUserData.m
+++ User Data Challenge Solution/KXUserData.m
@@ -17,25 +17,25 @@
              USER_EMAIL:@"musha@hip.me",
              USER_PASSWORD:@"password",
              USER_AGE:@26,
-             USER_IMAGE:[UIImage imageNamed:@"person1.jpeg"]},
+             USER_IMAGE:[UIImage imageNamed:@"person1.jpg"]},
   
            @{USER_NAME: @"Chow Fa",
              USER_EMAIL:@"chow.fa@mac.com",
              USER_PASSWORD:@"好笑",
              USER_AGE:@13,
-             USER_IMAGE:[UIImage imageNamed:@"person2.jpeg"]},
+             USER_IMAGE:[UIImage imageNamed:@"person2.jpg"]},
   
            @{USER_NAME: @"I Pod",
              USER_EMAIL:@"ipod@apple.com",
              USER_PASSWORD:@"01101001011100000110111101100100",
              USER_AGE:@11,
-             USER_IMAGE:[UIImage imageNamed:@"person3.jpeg"]},
+             USER_IMAGE:[UIImage imageNamed:@"person3.jpg"]},
   
            @{USER_NAME: @"Henry VIII",
              USER_EMAIL:@"overlord@england.co.uk",
              USER_PASSWORD:@"maharajah4life",
              USER_AGE:@557,
-             USER_IMAGE:[UIImage imageNamed:@"person4.jpeg"]}
+             USER_IMAGE:[UIImage imageNamed:@"person4.jpg"]}
            ];
 }
 @end
